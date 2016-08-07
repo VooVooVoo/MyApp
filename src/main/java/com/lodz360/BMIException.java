03fa0bfc839278d2068809f0760eac47dde0f126
michalinab <michalina.bartosik@gmail.com>
2016-08-07 15:16:39 +0200

    Dziedziczenie klas będu - wersja z bugumi

--- src/main/java/com/lodz360/BMIException.java
+++ src/main/java/com/lodz360/BMIException.java
@@ -1,7 +1,19 @@
 package com.lodz360;
 
+import com.lodz360.User;
+import com.lodz360.MyApp;
+
 /**
  * Created by michalina on 05/08/16.
  */
 public class BMIException extends Throwable {
+    public static void main(String[] args) {
+        try {
+            (user.checkbmi);
+        } catch (BMIToLowException toLow) {
+            System.out.println("Wpisałeś głupoty, albo jesteś tak chudy, że już nic Ci nie pomoże");
+        } catch (BMIToHighException toHigh) {
+            System.out.println("Za duże BMI");
+        }
+    }
 }
