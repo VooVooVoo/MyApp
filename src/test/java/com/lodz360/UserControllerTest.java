5855f2fcf4cb94fa84dfe3ec69f6c447c31644c4
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-08-23 14:09:05 +0200

    Using argument captor to check values in test

--- src/test/java/com/lodz360/UserControllerTest.java
+++ src/test/java/com/lodz360/UserControllerTest.java
@@ -1,12 +1,19 @@
 package com.lodz360;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.mockito.Mockito.*;
 
 /**
@@ -40,16 +47,25 @@ public class UserControllerTest {
         UserRepository userRepository = mock(UserRepository.class);
         ProductRepository productRepository = mock(ProductRepository.class);
         UserController userController = new UserController(productRepository, userRepository);
-        Product product = new Product("name",1,2,3);
-        List<Product> products = new ArrayList<>();
-        products.add(product);
-        when(productRepository.getAllProducts()).thenReturn(products);
-
+        //Product product = new Product("name",1,2,3);
 
         //When
-        userController.product("name",1,2,3);
+        userController.product("Milk",1,2,3);
+
         //Then
-        verify(productRepository).dodaj(any());
+        ArgumentCaptor<Product> captor = ArgumentCaptor.forClass(Product.class);
+        verify(productRepository).dodaj(captor.capture());
+        Product actualArgument = captor.getValue();
+
+        assertEquals("Milk", actualArgument.getName());
+        assertThat(actualArgument.getName()).isEqualTo("Milk");
+        Assert.assertThat(actualArgument, equalTo("Milk"));
+        assertTrue("Milk".equals(actualArgument.getName()));
+
+        assertThat(actualArgument).isEqualToComparingFieldByField(new Product("Milk",1,2,3));
+
+
+
 
     }
 }
